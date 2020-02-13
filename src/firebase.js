1e399d370ce2a7fb5e4b20e20aa891ba7168d359
qriscilla <pri.whang@gmail.com>
2020-02-13 08:13:22 -0500

    Left off at 40:12 Completed setting up Firebase database

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectID: '',
-    storageBucket: '',
-    messageSenderID: '',
-    apiId: '',
+    apiKey: "AIzaSyBc59QjeE7A8UyyMs1nI-Yk3WSlzoJxC3Q",
+    authDomain: "todoist-knockoff.firebaseapp.com",
+    databaseURL: "https://todoist-knockoff.firebaseio.com",
+    projectId: "todoist-knockoff",
+    storageBucket: "todoist-knockoff.appspot.com",
+    messagingSenderId: "1097813861045",
+    appId: "1:1097813861045:web:37a0bdc8259d9e049114fc"
 })
 
 export {firebaseConfig as firebase};
