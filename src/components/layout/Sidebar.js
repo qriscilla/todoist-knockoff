226c47b3b66c9261072789097764aaee578619d6
qriscilla <pri.whang@gmail.com>
2020-02-12 22:39:42 -0500

    Left off at 32:05 Installed firebase and nonde sass

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -4,8 +4,27 @@ import {FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar} from 'react-ico
 export const Sidebar = () => 
 <div className='sidebar' data-testid='sidebar'>
     <ul className='sidebar__generic'>
-        <li><span><FaInbox /></span><span>Inbox</span></li>
-        <li>Today</li>
-        <li>Next 7 days</li>
+        <li>
+            <span><FaInbox /></span>
+            <span>Inbox</span>
+        </li>
+        <li>
+            <span><FaRegCalendar /></span>
+            <span>Today</span>
+        </li>
+        <li>
+            <span><FaRegCalendarAlt /></span>
+            <span>Next 7 days</span>
+        </li>
     </ul>
+
+    <div className='sidebar__middle'>
+        <span><FaChevronDown /></span>
+        <h2>Projects</h2>
+    </div>
+    
+    <ul className='sidebar__projects'>Projects will be here!</ul>
+
+    Add Project Component here!
+
 </div>;
