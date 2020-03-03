188323f7b80e0448a41bff4e6fce6cdf8fd89731
qriscilla <pri.whang@gmail.com>
2020-03-02 19:15:13 -0500

    Can add task and assign it to a project and set a deadline

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,6 +4,8 @@ import moment from 'moment';
 import { firebase } from '../firebase';
 import { useSelectedProjectValue } from '../context';
 import { render } from '@testing-library/react';
+import { ProjectOverlay } from './ProjectOverlay';
+import { TaskDate } from './TaskDate';
 
 export const AddTask = ({
     showAddTaskMain = true, 
@@ -71,6 +73,80 @@ export const AddTask = ({
                 </div>
             )}
 
+            {(showMain || showQuickAddTask)
+                && (
+                    <div className='add-task__main' data-testid='add-task-main'>
+                        {showQuickAddTask && (
+                            <>
+                                <div data-testid='quick-add-task'>
+                                    <h2 className='header'>Quick Add Task</h2>
+                                    <span
+                                        className='add-task__cancel-x'
+                                        data-testid='add-task-quick-cancel'
+                                        onClick = {() => {
+                                            setShowMain(false);
+                                            setShowProjectOverlay(false);
+                                            setShowQuickAddTask(false);
+                                        }}
+                                    >
+                                        X
+                                    </span>
+                                </div>
+                            </>
+                        )}
+                        <ProjectOverlay 
+                            setProject={setProject} 
+                            showProjectOverlay={showProjectOverlay} 
+                            setShowProjectOverlay={setShowProjectOverlay} 
+                        />
+                        <TaskDate 
+                            setTaskDate={setTaskDate} 
+                            showTaskDate={showTaskDate}
+                            setShowTaskDate={setShowTaskDate}
+                        />
+                        <input
+                            className='add-task__content'
+                            data-testid='add-task-content'
+                            type='text'
+                            value={task}
+                            onChange={e => setTask(e.target.value)}
+                        />
+                        <button
+                            type='button'
+                            className='add-task__submit'
+                            data-testid='add-task'
+                            onClick={() => addTask()}
+                        >
+                            Add Task
+                        </button>
+                        {!showQuickAddTask && (
+                            <span 
+                                className='add-task__cancel'
+                                data-testid='add-task-main-cancel'
+                                onClick = {() => {
+                                    setShowMain(false);
+                                    setShowProjectOverlay(false);
+                                }}
+                            >
+                                Cancel
+                            </span>
+                        )}
+                        <span
+                            className='add-task__project'
+                            data-testid='show-project-overlay'
+                            onClick = {() => setShowProjectOverlay(!showProjectOverlay)}
+                        >
+                            <FaRegListAlt />
+                        </span>
+                        <span
+                            className='add-task__date'
+                            data-testid='show-task-data-overlay'
+                            onClick = {() => setShowTaskDate(!showTaskDate)}
+                        >
+                            <FaRegCalendarAlt />
+                        </span>
+                    </div>
+                )}
         </div>
     );
 
