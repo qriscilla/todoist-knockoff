d41deb75b408bbbc1a5241db0f994a54691168d9
qriscilla <pri.whang@gmail.com>
2020-03-08 20:50:54 -0400

    Updates

---
+++ precache-manifest.dad28d66affc8435c6d226e2a23b6220.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6d157bc44e72c44de1f61fc4774ba3",
+    "url": "/todoist-knockoff/index.html"
+  },
+  {
+    "revision": "47f605e458838a1a8006",
+    "url": "/todoist-knockoff/static/css/main.95eb2b87.chunk.css"
+  },
+  {
+    "revision": "994149201c8f3332eb21",
+    "url": "/todoist-knockoff/static/js/2.870eab3e.chunk.js"
+  },
+  {
+    "revision": "47f605e458838a1a8006",
+    "url": "/todoist-knockoff/static/js/main.1685a319.chunk.js"
+  },
+  {
+    "revision": "54faf4affea7cf6d4dc8",
+    "url": "/todoist-knockoff/static/js/runtime~main.48d219f2.js"
+  }
+]);
